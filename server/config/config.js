20751fb3c6b0a8230b0560c27812ef0a0007a1ce
FranLP96 <franco.peralta7@hotmail.com>
2020-08-29 17:13:20 -0300

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+// Puerto
+process.env.PORT = process.env.PORT || 3000;
+
+// Vencimiento del Token
+process.env.CADUCIDAD_TOKEN = '48h';
+
+// SEED de autenticacion
+process.env.SEED = 'este-es-el-seed-desarrollo';
---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,61 @@
+const jwt = require('jsonwebtoken');
+
+// Verifica Token
+let verificaToken = (req, res, next) => {
+    let token = req.get('token');
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no valido'
+                }
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+    });
+};
+
+// Verifica AdminRole
+let verificaAdmin_Role = (req, res, next) => {
+    let usuario = req.usuario;
+
+    if (usuario.role === 'ADMIN_ROLE') {
+        next();
+    } else {
+        res.json({
+            ok: false,
+            err: {
+                message: 'El usuario no es administrador'
+            }
+        });
+    }
+}
+
+// Verifica token para imagen
+let verificaTokenImg = (req, res, next) => {
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no valido'
+                }
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+    });
+}
+
+module.exports = {
+    verificaToken,
+    verificaAdmin_Role,
+    verificaTokenImg
+}
